0e8b38c5b5c52382462dc53459b73d41ab81d724
Naimur53 <naimurrhman53@gmail.com>
2021-07-23 22:21:19 +0600

    fully done

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ window.addEventListener("scroll", function () {
     moon.style.top = value * 1 + "px";
     mountains_behind.style.top = value * 0.5 + "px";
     mountains_front.style.top = value * 0 + "px";
-    header_text.style.marginRight = value * 3 + "px";
+    header_text.style.marginRight = value * 2.5 + "px";
     header_text.style.marginTop = value * 1.5 + "px";
     btn.style.marginTop = value * 1.5 + "px";
 })
